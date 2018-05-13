c575074319edfdc1b6c49ce9f0d56daeb138e348
Misael Gomes <misaelweb@hotmail.com>
2018-05-13 17:14:19 -0300

    Update EstimateController.php

--- app/code/community/WebDevlopers/ProductPageShipping/controllers/EstimateController.php
+++ app/code/community/WebDevlopers/ProductPageShipping/controllers/EstimateController.php
@@ -17,7 +17,8 @@ class WebDevlopers_ProductPageShipping_EstimateController extends Mage_Catalog_P
             $product->setAddToCartInfo((array) $this->getRequest()->getPost());
             $estimate->setProduct($product);
             $addressInfo = $this->getRequest()->getPost('estimate');
-            $tamanho = $this->getRequest()->getPost('super_attribute[203]');
+            $tamanho = $this->getRequest()->getPost('super_attribute');
+            Mage::log("Tamanho");
             Mage::log($tamanho);
             $estimate->setAddressInfo((array) $addressInfo);
             $block->getSession()->setFormValues($addressInfo);
